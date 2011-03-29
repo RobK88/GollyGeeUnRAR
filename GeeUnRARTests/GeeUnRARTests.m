3b17d64da59fd54d04798f91a568b52a2e03c730
Giuliano A. Montecarlo <gamontecarlo@77-22-108-135-dynip.superkabel.de>
2011-03-29 03:33:44 +0200

    added tests

---
+++ GeeUnRARTests/GeeUnRARTests.m
@@ -0,0 +1,48 @@
+//
+//  GeeUnRARTests.m
+//  GeeUnRARTests
+//
+//  Created by Giuliano A. Montecarlo on 3/27/11.
+//  Copyright 2011 Giuliano A. Montecarlo. All rights reserved.
+//
+//  GeeUnRAR - Mac OS X GUI for unrar
+//  Copyright (C) 2011  Giuliano A. Montecarlo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#import "GeeUnRARTests.h"
+
+
+@implementation GeeUnRARTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GeeUnRARTests");
+}
+
+@end
